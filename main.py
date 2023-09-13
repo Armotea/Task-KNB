baf21bab72554e4c270a99954d3017abe15b2f76
Armotea <gong322@mail.ru>
2023-09-13 20:26:18 +0300

    Добавил определение победителя.

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ rus_choice = int(input("Знак выбирает Руслан: "))
 if ((tim_choice > 3) or (tim_choice < 1)) or ((rus_choice > 3) or (rus_choice < 1)):
     print("Вы ввели некорректное значение!!!")
 
-
-
-
+if tim_choice == 1 and rus_choice == 3:
+    print("Победил Тимур!!!")
+elif tim_choice == 2 and rus_choice == 1:
+    print("Победил Тимур!!!")
+elif tim_choice == 3 and rus_choice == 2:
+    print("Победил Тимур!!!")
+elif tim_choice == rus_choice:
+    print("Ничья!!!")
+else:
+    print("Победил Руслан!!!")
